ee3a538526998e71a4789694c26e595485e58077
ritesh.k3 <ritesh.k@redbasil.in>
2023-03-22 07:40:12 +0530

    task model updated

--- models/user.model.js
+++ models/user.model.js
@@ -37,6 +37,10 @@ const userSchema = mongoose.Schema({
         trim: true,
         enum: ['ACTIVE', 'INACTIVE']
     },
+    role: {
+        type: String,
+        trim: true
+    },
     createdBy: {
         type: String,
         trim: true,
