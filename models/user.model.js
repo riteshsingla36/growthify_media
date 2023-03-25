8a2e1317e84fb9fd43fc423acfd4486aacc5add9
ritesh.k3 <ritesh.k@redbasil.in>
2023-03-25 14:41:33 +0530

    updated user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,58 +1,82 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: true
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
     },
     userId: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: true
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
     },
     phoneNo: {
-        type: Number,
-        max: 9999999999,
-        trim: true,
-        unique: true,
-        required: true
+      type: Number,
+      max: 9999999999,
+      trim: true,
+      unique: true,
+      required: true,
     },
     userType: {
-        type: String,
-        trim: true,
-        enum: ['Employee', 'Client'],
-        required: true
+      type: String,
+      trim: true,
+      enum: ['Employee', 'Client'],
+      required: true,
     },
     status: {
-        type: String,
-        trim: true,
-        enum: ['ACTIVE', 'INACTIVE']
+      type: String,
+      trim: true,
+      enum: ['ACTIVE', 'INACTIVE'],
     },
     role: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true,
+    },
+    projects: {
+      type: String,
+      trim: true,
+    },
+    billingAddress: {
+      type: String,
+      trim: true,
+    },
+    GSTIN: {
+      type: String,
+      trim: true,
+    },
+    billingPhoneNo: {
+      type: String,
+      trim: true,
+    },
+    brandName: {
+      type: String,
+      trim: true,
+    },
+    stateCode: {
+      type: String,
+      trim: true,
     },
     createdBy: {
-        type: String,
-        trim: true,
-        required: true
-    }
-
-},
-    {
-        versionKey: false,
-        timestamps: true,
-    }
-)
+      type: String,
+      trim: true,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 const User = mongoose.models.user || mongoose.model('user', userSchema);
 
