38105388122747af068dab78ec64e335d1bf08b4
ritesh.k3 <ritesh.k@redbasil.in>
2023-04-04 18:06:12 +0530

    updated update user method

---
+++ src/pages/api/update_user.js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import User from 'models/user.model';
+import { connectDB } from 'setup/connectDb';
+
+export default async function handler(req, res) {
+  if (req.method === 'PATCH') {
+    const userType = req.query.userType;
+    try {
+      await connectDB();
+      let user;
+      if(userType) {
+        user = await User.findOneAndUpdate({email: email}, req.body, {runValidators: true});
+        return res.status(200).json(user);
+      }
+      return res.status(200).json({})
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  res.status(400).json({ data: 'Method not allowed' });
+};
