8db197e6dffce160d69b7c89884a9c71956115fa
ritesh.k3 <ritesh.k@redbasil.in>
2023-04-04 15:42:20 +0530

    created get user method

---
+++ src/pages/api/get_user.js
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import User from 'models/user.model';
+import { connectDB } from 'setup/connectDb';
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const name = req.query.name;
+    try {
+      await connectDB();
+      let user;
+      if(name) {
+        user = await User.find({name: name});
+      }
+      else {
+        user = await User.find({});
+      }
+      return res.status(200).json(user);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  res.status(400).json({ data: 'Method not allowed' });
+};
