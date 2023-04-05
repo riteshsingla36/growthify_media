81ee8cbb343c0b8d1d45e63db7cff0d087d808b8
ritesh.k3 <ritesh.k@redbasil.in>
2023-04-05 18:15:17 +0530

    added update task method

---
+++ src/pages/api/update_task.js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import Task from 'models/task.model';
+import { connectDB } from 'setup/connectDb';
+
+export default async function handler(req, res) {
+  if (req.method === 'PATCH') {
+    const taskId = req.query.taskId;
+    try {
+      await connectDB();
+      let task;
+      if(id) {
+        task = await Task.findOneAndUpdate({_id: taskId}, req.body, {runValidators: true});
+        return res.status(200).json(task);
+      }
+      return res.status(200).json({})
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  res.status(400).json({ data: 'Method not allowed' });
+};
