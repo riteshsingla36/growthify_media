6cc11532f615d487c2fcd6885547555bc1cb4206
ritesh.k3 <ritesh.k@redbasil.in>
2023-04-16 03:43:26 +0530

    added login page and chips on tasks page

--- src/pages/all_tasks.jsx
+++ src/pages/all_tasks.jsx
@@ -5,179 +5,408 @@ import { Column } from 'primereact/column';
 import { InputText } from 'primereact/inputtext';
 import { Dropdown } from 'primereact/dropdown';
 import { Tag } from 'primereact/tag';
-import "primereact/resources/themes/lara-light-blue/theme.css";     
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";     
 import axios from 'axios';
 
 const AllTasks = (props) => {
-    const [filters, setFilters] = useState({
-        assignor: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        assignee: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        client: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        priority: { value: null, matchMode: FilterMatchMode.EQUALS },
-        status: { value: null, matchMode: FilterMatchMode.EQUALS },
-    });
+  const [filters, setFilters] = useState({
+    assignor: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    assignee: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    client: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    priority: { value: null, matchMode: FilterMatchMode.EQUALS },
+    status: { value: null, matchMode: FilterMatchMode.EQUALS },
+  });
 
-    const [statuses] = useState(['inProgress', 'pending', 'completed']);
-    const [priorities] = useState(['low', 'medium', 'high']);
-    const [rowClick, setRowClick] = useState(true);
-    const [selectedProduct, setSelectedProduct] = useState(null);
+  const [statuses] = useState(['inProgress', 'pending', 'completed']);
+  const [priorities] = useState(['low', 'medium', 'high']);
+  const [rowClick, setRowClick] = useState(true);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
-    const statusItemTemplate = (option) => {
-        return <Tag value={option} severity={getSeverity(option)} />;
-    };
+  const statusItemTemplate = (option) => {
+    return <Tag value={option} severity={getSeverity(option)} />;
+  };
 
-
-const getSeverity = (status) => {
-  switch (status) {
+  const getSeverity = (status) => {
+    switch (status) {
       case 'pending':
-          return 'danger';
+        return 'danger';
       case 'completed':
-          return 'success';
+        return 'success';
       case 'inProgress':
-          return 'warning';
+        return 'warning';
       case 'low':
         return 'success';
       case 'medium':
         return 'warning';
       case 'high':
         return 'danger';
-  }
-};
-
-const deadlinedateBodyTemplate = (rowData) => {
-  return formatDate(rowData.deadline);
-};
-const createdAtdateBodyTemplate = (rowData) => {
-  return formatDate(rowData.deadline);
-};
-const updatedAtdateBodyTemplate = (rowData) => {
-  return formatDate(rowData.deadline);
-};
-
-const formatDate = (value) => {
-  const date = new Date(value);
-  const localizedDateString = date.toLocaleDateString();
-  return localizedDateString;
-};
-
-  const isSelectable = (data) => data.status != "completed";
-
-  const isRowSelectable = (event) => (event.data ? isSelectable(event.data) : true);
-
-  const rowClassName = (data) => (isSelectable(data) ? '' : 'bg-success text-white p-disabled');
-
-    const statusBodyTemplate = (rowData) => {
-      return <Tag value={rowData.status} severity={getSeverity(rowData.status)} />;
+    }
   };
 
-    const priorityBodyTemplate = (rowData) => {
-      return <Tag value={rowData.priority} severity={getSeverity(rowData.priority)} />;
+  const deadlinedateBodyTemplate = (rowData) => {
+    return formatDate(rowData.deadline);
+  };
+  const createdAtdateBodyTemplate = (rowData) => {
+    return formatDate(rowData.deadline);
+  };
+  const updatedAtdateBodyTemplate = (rowData) => {
+    return formatDate(rowData.deadline);
   };
 
-    const statusRowFilterTemplate = (options) => {
-        return (
-            <Dropdown value={options.value} options={statuses} onChange={(e) => options.filterApplyCallback(e.value)} itemTemplate={statusItemTemplate} placeholder="Select One" className="p-column-filter" showClear style={{ minWidth: '12rem' }} />
-        );
-    };
+  const formatDate = (value) => {
+    const date = new Date(value);
+    const localizedDateString = date.toLocaleDateString('en-IN');
+    return localizedDateString;
+  };
 
-    const priorityRowFilterTemplate = (options) => {
-      return (
-          <Dropdown value={options.value} options={priorities} onChange={(e) => options.filterApplyCallback(e.value)} itemTemplate={statusItemTemplate} placeholder="Select One" className="p-column-filter" showClear style={{ minWidth: '12rem' }} />
-      );
+  const isSelectable = (data) => data.status != 'completed';
+
+  const isRowSelectable = (event) =>
+    event.data ? isSelectable(event.data) : true;
+
+  const rowClassName = (data) =>
+    isSelectable(data) ? '' : 'bg-success text-white p-disabled';
+
+  const statusBodyTemplate = (rowData) => {
+    return (
+      <Tag value={rowData.status} severity={getSeverity(rowData.status)} />
+    );
+  };
+
+  const priorityBodyTemplate = (rowData) => {
+    return (
+      <Tag value={rowData.priority} severity={getSeverity(rowData.priority)} />
+    );
+  };
+
+  const statusRowFilterTemplate = (options) => {
+    return (
+      <Dropdown
+        value={options.value}
+        options={statuses}
+        onChange={(e) => options.filterApplyCallback(e.value)}
+        itemTemplate={statusItemTemplate}
+        placeholder="Select One"
+        className="p-column-filter"
+        showClear
+        style={{ minWidth: '12rem' }}
+      />
+    );
+  };
+
+  const priorityRowFilterTemplate = (options) => {
+    return (
+      <Dropdown
+        value={options.value}
+        options={priorities}
+        onChange={(e) => options.filterApplyCallback(e.value)}
+        itemTemplate={statusItemTemplate}
+        placeholder="Select One"
+        className="p-column-filter"
+        showClear
+        style={{ minWidth: '12rem' }}
+      />
+    );
   };
 
   const textEditor = (options) => {
-    return <InputText type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} />;
-};
-
-const statusEditor = (options) => {
-  return (
-      <Dropdown
-          value={options.value}
-          options={statuses}
-          onChange={(e) => options.editorCallback(e.value)}
-          placeholder="Select a Status"
-          itemTemplate={(option) => {
-              return <Tag value={option} severity={getSeverity(option)}></Tag>;
-          }}
-      />
-  );
-};
-
-const clientEditor = (options) => {
-  return (
-      <Dropdown
-          value={options.value}
-          options={Object.keys(props.clients)}
-          onChange={(e) => options.editorCallback(e.value)}
-          placeholder="Select a Client"
-      />
-  );
-};
-
-const employeesEditor = (options) => {
-  return (
-      <Dropdown
-          value={options.value}
-          options={Object.keys(props.employees)}
-          onChange={(e) => options.editorCallback(e.value)}
-          placeholder="Select an Employee"
-      />
-  );
-};
-
-const onRowEditComplete = (e) => {
-
-
-};
-
-
-
     return (
-        <div className="card">
-            <DataTable scrollable isDataSelectable={isRowSelectable} rowClassName={rowClassName} sortField="createdAt" removableSort sortOrder={-1} scrollHeight='90vh' showGridlines editMode='row' onRowEditComplete={onRowEditComplete} value={props.tasks} paginator rows={10} dataKey="_id" filters={filters} filterDisplay="row"
-                    globalFilterFields={['name', 'country.name', 'representative.name', 'status']} emptyMessage="No customers found."  tableStyle={{ minWidth: '50rem' }}>
-                      {/* <Column selectionMode="multiple" headerStyle={{ width: '3rem' }} field='_id'></Column> */}
-                <Column rowEditor headerStyle={{ width: '10%', minWidth: '2rem' }} bodyStyle={{ textAlign: 'center' }}></Column>
-                <Column field="assignor" header="Assignor" editor={(options) => employeesEditor(options)} filter filterPlaceholder="Search by name" style={{ minWidth: '12rem' }} />
-                <Column field="assignee" header="Assignee" editor={(options) => employeesEditor(options)} filter filterPlaceholder="Search by name" style={{ minWidth: '12rem' }} />
-                <Column field="client" header="Client" filter filterPlaceholder="Search by name"  editor={(options) => clientEditor(options)} style={{ minWidth: '12rem' }} />
-                <Column field="status" header="Status" editor={(options) => statusEditor(options)} showFilterMenu={false} filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '12rem' }} body={statusBodyTemplate} filter filterElement={statusRowFilterTemplate} />
-                <Column field="priority" header="Priority" showFilterMenu={false} filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '12rem' }} body={priorityBodyTemplate} filter filterElement={priorityRowFilterTemplate} />
-                <Column field="deadline" body={deadlinedateBodyTemplate} header="Deadline" sortable style={{ minWidth: '12rem' }} />
-                <Column field="description" header="Description"  style={{ minWidth: '12rem' }} />
-                <Column field="supportingLink" header="Supporting Link"  style={{ minWidth: '12rem' }} />
-                <Column field="supportingRemarks" header="Supporting Remarks"  style={{ minWidth: '12rem' }} />
-                <Column field="createdBy" header="CreatedBy"  style={{ minWidth: '12rem' }} />
-                <Column field="updatedBy" header="UpdatedBy"  style={{ minWidth: '12rem' }} />
-                <Column field="createdAt" body={createdAtdateBodyTemplate} header="CreatedAt" sortable style={{ minWidth: '12rem' }} />
-                <Column field="updatedAt" body={updatedAtdateBodyTemplate} header="UpdatedAt" sortable style={{ minWidth: '12rem' }} />
-            </DataTable>
-        </div>
+      <InputText
+        type="text"
+        value={options.value}
+        onChange={(e) => options.editorCallback(e.target.value)}
+      />
     );
-}
+  };
+
+  const statusEditor = (options) => {
+    return (
+      <Dropdown
+        value={options.value}
+        options={statuses}
+        onChange={(e) => options.editorCallback(e.value)}
+        placeholder="Select a Status"
+        itemTemplate={(option) => {
+          return <Tag value={option} severity={getSeverity(option)}></Tag>;
+        }}
+      />
+    );
+  };
+
+  const clientEditor = (options) => {
+    return (
+      <Dropdown
+        value={options.value}
+        options={Object.keys(props.clients)}
+        onChange={(e) => options.editorCallback(e.value)}
+        placeholder="Select a Client"
+      />
+    );
+  };
+
+  const employeesEditor = (options) => {
+    return (
+      <Dropdown
+        value={options.value}
+        options={Object.keys(props.employees)}
+        onChange={(e) => options.editorCallback(e.value)}
+        placeholder="Select an Employee"
+      />
+    );
+  };
+
+  const onRowEditComplete = (e) => {};
+
+  return (
+    <>
+      <div className="grid">
+        <div className="col-12 md:col-6 lg:col-3">
+          <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
+            <div className="flex justify-content-between mb-3">
+              <div>
+                <span className="block text-500 font-medium mb-3">
+                  All Tasks
+                </span>
+                <div className="text-900 font-medium text-xl">
+                  {props.counts['all']}
+                </div>
+              </div>
+              <div
+                className="flex align-items-center justify-content-center bg-blue-100 border-round"
+                style={{ width: '2.5rem', height: '2.5rem' }}
+              >
+                <i className="pi pi-shopping-cart text-blue-500 text-xl"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 md:col-6 lg:col-3">
+          <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
+            <div className="flex justify-content-between mb-3">
+              <div>
+                <span className="block text-500 font-medium mb-3">
+                  Pending Tasks
+                </span>
+                <div className="text-900 font-medium text-xl">
+                  {props.counts['pending']}
+                </div>
+              </div>
+              <div
+                className="flex align-items-center justify-content-center bg-orange-100 border-round"
+                style={{ width: '2.5rem', height: '2.5rem' }}
+              >
+                <i className="pi pi-map-marker text-orange-500 text-xl"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 md:col-6 lg:col-3">
+          <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
+            <div className="flex justify-content-between mb-3">
+              <div>
+                <span className="block text-500 font-medium mb-3">
+                  In Progress Tasks
+                </span>
+                <div className="text-900 font-medium text-xl">
+                  {props.counts['inProgress']}
+                </div>
+              </div>
+              <div
+                className="flex align-items-center justify-content-center bg-cyan-100 border-round"
+                style={{ width: '2.5rem', height: '2.5rem' }}
+              >
+                <i className="pi pi-inbox text-cyan-500 text-xl"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 md:col-6 lg:col-3">
+          <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
+            <div className="flex justify-content-between mb-3">
+              <div>
+                <span className="block text-500 font-medium mb-3">
+                  Completed Tasks
+                </span>
+                <div className="text-900 font-medium text-xl">
+                  {props.counts['completed']}
+                </div>
+              </div>
+              <div
+                className="flex align-items-center justify-content-center bg-purple-100 border-round"
+                style={{ width: '2.5rem', height: '2.5rem' }}
+              >
+                <i className="pi pi-comment text-purple-500 text-xl"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="card">
+        <DataTable
+          scrollable
+          isDataSelectable={isRowSelectable}
+          rowClassName={rowClassName}
+          sortField="createdAt"
+          removableSort
+          sortOrder={-1}
+          scrollHeight="90vh"
+          showGridlines
+          editMode="row"
+          onRowEditComplete={onRowEditComplete}
+          value={props.tasks}
+          paginator
+          rows={10}
+          dataKey="_id"
+          filters={filters}
+          filterDisplay="row"
+          globalFilterFields={[
+            'name',
+            'country.name',
+            'representative.name',
+            'status',
+          ]}
+          emptyMessage="No customers found."
+          tableStyle={{ minWidth: '50rem' }}
+        >
+          <Column
+            rowEditor
+            headerStyle={{ width: '10%', minWidth: '2rem' }}
+            bodyStyle={{ textAlign: 'center' }}
+          ></Column>
+          <Column
+            field="assignor"
+            header="Assignor"
+            editor={(options) => employeesEditor(options)}
+            filter
+            filterPlaceholder="Search by name"
+            style={{ minWidth: '12rem' }}
+          />
+          <Column
+            field="assignee"
+            header="Assignee"
+            editor={(options) => employeesEditor(options)}
+            filter
+            filterPlaceholder="Search by name"
+            style={{ minWidth: '12rem' }}
+          />
+          <Column
+            field="client"
+            header="Client"
+            filter
+            filterPlaceholder="Search by name"
+            editor={(options) => clientEditor(options)}
+            style={{ minWidth: '12rem' }}
+          />
+          <Column
+            field="status"
+            header="Status"
+            editor={(options) => statusEditor(options)}
+            showFilterMenu={false}
+            filterMenuStyle={{ width: '14rem' }}
+            style={{ minWidth: '12rem' }}
+            body={statusBodyTemplate}
+            filter
+            filterElement={statusRowFilterTemplate}
+          />
+          <Column
+            field="priority"
+            header="Priority"
+            showFilterMenu={false}
+            filterMenuStyle={{ width: '14rem' }}
+            style={{ minWidth: '12rem' }}
+            body={priorityBodyTemplate}
+            filter
+            filterElement={priorityRowFilterTemplate}
+          />
+          <Column
+            field="deadline"
+            body={deadlinedateBodyTemplate}
+            header="Deadline"
+            sortable
+            style={{ minWidth: '12rem' }}
+          />
+          <Column
+            field="description"
+            header="Description"
+            style={{ minWidth: '12rem' }}
+          />
+          <Column
+            field="supportingLink"
+            header="Supporting Link"
+            style={{ minWidth: '12rem' }}
+          />
+          <Column
+            field="supportingRemarks"
+            header="Supporting Remarks"
+            style={{ minWidth: '12rem' }}
+          />
+          <Column
+            field="createdBy"
+            header="CreatedBy"
+            style={{ minWidth: '12rem' }}
+          />
+          <Column
+            field="updatedBy"
+            header="UpdatedBy"
+            style={{ minWidth: '12rem' }}
+          />
+          <Column
+            field="createdAt"
+            body={createdAtdateBodyTemplate}
+            header="CreatedAt"
+            sortable
+            style={{ minWidth: '12rem' }}
+          />
+          <Column
+            field="updatedAt"
+            body={updatedAtdateBodyTemplate}
+            header="UpdatedAt"
+            sortable
+            style={{ minWidth: '12rem' }}
+          />
+        </DataTable>
+      </div>
+    </>
+  );
+};
 
 export async function getServerSideProps(context) {
-  const tasks = await axios.get('https://growthify-media.vercel.app/api/get_tasks');
-  const usersData = await axios.get('https://growthify-media.vercel.app/api/get_users');
+  const tasks = await axios.get(
+    'https://growthify-media.vercel.app/api/get_tasks'
+  );
+  const usersData = await axios.get(
+    'https://growthify-media.vercel.app/api/get_users'
+  );
   let clients = {};
   let employees = {};
-  for(let i = 0; i < usersData.data.length; i++) {
+  for (let i = 0; i < usersData.data.length; i++) {
     const user = usersData.data[i];
-    if(user.userType === 'Employee') {
-      if(!employees[user.name]){
+    if (user.userType === 'Employee') {
+      if (!employees[user.name]) {
         employees[user.name] = user;
       }
-    }else {
-      if(!clients[user.name]){
+    } else {
+      if (!clients[user.name]) {
         clients[user.name] = user;
       }
     }
   }
+
+  var counts = { all: 0, pending: 0, inProgress: 0, completed: 0 };
+  counts.all = tasks.data.length;
+
+  for (let i = 0; i < tasks.data.length; i++) {
+    const task = tasks.data[i];
+    if (task.status === 'completed') {
+      counts.completed++;
+    } else if (task.status === 'pending') {
+      counts.pending++;
+    } else {
+      counts.inProgress++;
+    }
+  }
   return {
-    props: { tasks: tasks.data, clients, employees },
+    props: { tasks: tasks.data, clients, employees, counts },
   };
 }
 
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
+import { InputText } from 'primereact/inputtext';
+
+import { Checkbox } from 'primereact/checkbox';
+import { Button } from 'primereact/button';
 
 const Login = () => {
+  const login = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>Login Hello Ritesh</div>
-  )
-}
+    <div className="flex align-items-center justify-content-center">
+      <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+        <div className="text-center mb-5">
+          <img
+            src="https://growthifymedia.com/wp-content/uploads/2022/03/growthify-logo.png"
+            alt="hyper"
+            height={50}
+            width={400}
+            className="mb-3 m-auto"
+          />
+          <div className="text-900 text-3xl font-medium mb-3">Welcome Back</div>
+          {/* <span className="text-600 font-medium line-height-3">Don't have an account?</span>
+            <a className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</a> */}
+        </div>
 
-export default Login
+        <form method="post" onSubmit={login}>
+          <label htmlFor="email" className="block text-900 font-medium mb-2">
+            Email
+          </label>
+          <InputText
+            id="email"
+            type="text"
+            placeholder="Email address"
+            className="w-full mb-3"
+            required
+          />
+
+          <label htmlFor="password" className="block text-900 font-medium mb-2">
+            Password
+          </label>
+          <InputText
+            id="password"
+            type="password"
+            placeholder="Password"
+            className="w-full mb-3"
+            required
+          />
+
+          {/* <div className="flex align-items-center justify-content-between mb-6">
+                <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot your password?</a>
+            </div> */}
+
+          <Button
+            label="Sign In"
+            type="submit"
+            icon="pi pi-user"
+            className="w-full"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
