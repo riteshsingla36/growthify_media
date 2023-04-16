d2b5568a091b4be4134900a6cb21470b47ad3b87
deepu024 <ojashsharma024@gmail.com>
2023-04-16 18:32:09 +0530

    add some fields

--- src/pages/create_user.jsx
+++ src/pages/create_user.jsx
@@ -7,6 +7,7 @@ import { InputMask } from 'primereact/inputmask';
 import { ToggleButton } from 'primereact/togglebutton';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { Button } from 'primereact/button';
+import { Password } from 'primereact/password';
 
 
 const CreateTask = (props) => {
@@ -14,6 +15,13 @@ const CreateTask = (props) => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState(null);
+    const [address, setAddress] = useState("");
+    const [gst, setGst] = useState("");
+    const [projects, setProjects] = useState("");
+    const [billingPhone, setBillingPhone] = useState("");
+    const [stateCode, setStateCode] = useState("");
+    const [brandName, setBrandName] = useState("");
+    const [password, setPassword] = useState("");
     const [role, setRole] = useState("");
     const [userId, setUserId] = useState("");
     const [selectedUserType, setSelectedUserType] = useState(null);
@@ -41,11 +49,9 @@ const CreateTask = (props) => {
     return (
         <>
             <h1 className='w-full m-auto text-center !bg-[#304562] text-white !pt-5'>CREATE USER</h1>
-            <div className='w-full h-[60vh] flex justify-center items-center !bg-[#304562]'>
+            <div className='w-full min-h-[60vh] flex justify-center items-center !bg-[#304562]'>
                 <form action="#" method='POST' onSubmit={createUser}>
                     <div className="card !grid border-0 grid-cols-2 gap-4 !bg-[#304562]">
-
-
                         <span className="p-float-label">
                             <InputText id="name" value={name} onChange={(e) => setName(e.target.value)} />
                             <label htmlFor="name">Name</label>
@@ -61,24 +67,56 @@ const CreateTask = (props) => {
                         </div>
 
                         <div className="card flex justify-content-center">
-                            <InputMask value={userId} onChange={(e) => setUserId(e.target.value)} mask="a**-999" placeholder="Phone No." />
+                            <InputMask value={userId} onChange={(e) => setUserId(e.target.value)} mask="a**-999" placeholder="User Id" />
                         </div>
 
-                        <ToggleButton onLabel="Active" offLabel="InActive" onIcon="pi pi-check" offIcon="pi pi-times"
-                            checked={status} onChange={(e) => setStatus(e.value)} className="w-9rem" />
+                        <Password value={password} placeholder='Password' onChange={(e) => setPassword(e.target.value)} toggleMask />
 
                         <span className="p-float-label">
                             <InputText id="role" value={role} onChange={(e) => setRole(e.target.value)} />
                             <label htmlFor="role">Role</label>
                         </span>
 
+                        <span className="p-float-label">
+                            <InputText id="projects" value={projects} onChange={(e) => setProjects(e.target.value)} />
+                            <label htmlFor="projects">Projects</label>
+                        </span>
+
                         <Dropdown value={selectedUserType} onChange={(e) => setSelectedUserType(e.value)} options={userTypes} optionLabel="name"
                             placeholder="Select User Type" className="w-full md:w-14rem" />
 
-                        <div>
+                        <span className="p-float-label">
+                            <InputText id="billaddress" value={address} onChange={(e) => setAddress(e.target.value)} />
+                            <label htmlFor="billaddress">Billing Address</label>
+                        </span>
 
-                            <Button label="Submit" className='w-fit' />
-                        </div>
+                        <span className="p-float-label">
+                            <InputText id="gstin" value={gst} onChange={(e) => setGst(e.target.value)} />
+                            <label htmlFor="gstin">GSTIN</label>
+                        </span>
+
+                        <span className="p-float-label">
+                            <InputText id="brandname" value={brandName} onChange={(e) => setBrandName(e.target.value)} />
+                            <label htmlFor="brandname">GSTIN</label>
+                        </span>
+
+                        <span className="p-float-label">
+                            <InputText id="billingphone" value={billingPhone} onChange={(e) => setBillingPhone(e.target.value)} />
+                            <label htmlFor="billingphone">Billing Phone No.</label>
+                        </span>
+
+                        <span className="p-float-label">
+                            <InputText id="statecode" value={stateCode} onChange={(e) => setStateCode(e.target.value)} />
+                            <label htmlFor="statecode">Billing Phone No.</label>
+                        </span>
+
+                        <span className="p-float-label">
+                            <ToggleButton onLabel="Active" offLabel="InActive" onIcon="pi pi-check" offIcon="pi pi-times"
+                                checked={status} onChange={(e) => setStatus(e.value)} placeholder='Status' className="w-9rem" />
+                        </span>
+
+
+                        <Button label="Submit" className='w-fit' />
                     </div>
 
                 </form>
