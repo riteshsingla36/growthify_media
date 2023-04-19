2bf97f03efa30557a1b781ed98cc1607989eea99
ritesh.k3 <ritesh.k@redbasil.in>
2023-04-19 22:56:00 +0530

    added inngest sample scheduler

---
+++ src/pages/api/inngest.js
@@ -0,0 +1,19 @@
+import { Inngest } from "inngest";
+import { serve } from "inngest/next";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ name: "My App" });
+
+const test = inngest.createFunction(
+  { name: "Weekly digest email" }, // The name of your function, used for observability.
+  { cron: "TZ=Asia/Kolkata 0 23 * * *" }, // The cron syntax for the function. TZ= is optional.
+
+  // This function will be called on the schedule above
+  async ({ step }) => {
+    return 'hello' // You can write whatever you want here.
+  }
+);
+
+  
+// Create an API that hosts zero functions
+export default serve(inngest, [test]);
