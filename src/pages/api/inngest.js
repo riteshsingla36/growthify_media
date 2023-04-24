1560ec3e67caeeb3f3c57a1da1e04cc735b1cfdc
ritesh.k3 <ritesh.k@redbasil.in>
2023-04-24 10:23:32 +0530

    added firebase module

--- src/pages/api/inngest.js
+++ src/pages/api/inngest.js
@@ -7,15 +7,15 @@ export const inngest = new Inngest({ name: "My App" });
 
 const test = inngest.createFunction(
   { name: "Weekly digest email" }, // The name of your function, used for observability.
-  { cron: "TZ=Asia/Kolkata 26 23 * * *" }, // The cron syntax for the function. TZ= is optional.
+  { cron: "TZ=Asia/Kolkata 33 23 * * *" }, // The cron syntax for the function. TZ= is optional.
 
   // This function will be called on the schedule above
   async ({ step }) => {
-    const res = await axios.post('/api/send_slack_message', {
-        channelName: 'ritesh_kumar_emp_001',
-        message: "This is an automated message send by the scheduler"
+    const res = await axios.post(`${process.env.BASE_URL}/api/send_slack_message`, {
+        "channelName": 'ritesh_kumar_emp_001',
+        "message": "This is an automated message send by the scheduler"
     })
-    return res // You can write whatever you want here.
+    return res.data // You can write whatever you want here.
   }
 );
   
